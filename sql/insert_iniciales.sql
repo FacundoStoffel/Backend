cb6032b680b904bf2179513c6a8d178888bfacdc
FacuStoffel <stoffelfacu29@gmail.com>
2023-10-20 11:10:58 -0300

    sql act

--- sql/insert_iniciales.sql
+++ sql/insert_iniciales.sql
@@ -1,21 +1,22 @@
 create database barberia;
 
-insert into usuario(mail,nombre,contrasena,apellido,id_rol) values
-('facu@gmail.com','Facu','1234','Stoffel',1) 
+insert into rol(nombre_rol)values ('Admin');
+insert into rol(nombre_rol)values ('Cliente');
 
-insert into rol(nombre_rol)values ('Admin')
-insert into rol(nombre_rol)values ('Cliente')
+-- la contra es 1234
+insert into usuario(mail,nombre,contrasena,apellido,id_rol) values
+('facu@gmail.com','Facu','$2b$10$sOd4aVQePkSgLufprvY3.eOmoqQFDkS.hCjzM9ANqobBo4pOwBlvO','Stoffel',1);
 
 insert into tipo_corte(corte_tipo, precio) values
-('corte','1500'),('barba','500'),('corte_barba','2000')
+('corte','1500'),('barba','500'),('corte_barba','2000');
 
 insert into metodo_pago(metodo) values
-('efectivo'), ('tarjeta')
+('efectivo'), ('tarjeta');
 
 insert into horario(hora) values
 ('08:00'),('08:30'),('09:00'),('09:30'),('10:00'),('10:30'),('11:00'),('11:30'),('12:00'),
 ('15:00'),('15:30'),('16:00'),('16:30'),('17:00'),('17:30'),('18:00'),('18:30'),
-('19:00'),('19:30'),('20:00'),('20:30')
+('19:00'),('19:30'),('20:00'),('20:30');
 
 select * from usuario
 select * from rol
