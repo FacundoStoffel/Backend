de62f122aecd32ef04df7fd0338dda7c4bc942ad
FacuStoffel <stoffelfacu29@gmail.com>
2024-04-24 19:45:45 -0300

    a

--- index.js
+++ index.js
@@ -41,17 +41,3 @@ app.listen(config.server.port, (err) => {
     }
 });
 
-
-app.get("/", (req, res) => {
-    const htmlResponse = `
-      <html>
-        <head>
-          <title>NodeJs y Express en Vercel</title>
-        </head>
-        <body>
-          <h1>Soy un proyecto Back end en vercel</h1>
-        </body>
-      </html>
-    `;
-    res.send(htmlResponse);
-  });
